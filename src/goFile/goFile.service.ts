5307a31eded5fc4f2439eeea3f9887e3f891021d
Jessica <jessbarbosa2000@gmail.com>
2022-10-27 11:48:16 -0300

    finalmente foi para o gofile

--- src/goFile/goFile.service.ts
+++ src/goFile/goFile.service.ts
@@ -3,41 +3,41 @@ import * as dotenv from 'dotenv';
 dotenv.config({path:'../.env'}); 
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import * as FormData from 'form-data'; 
-import * as fs from 'fs'; 
+import * as FormData from 'form-data';
+import * as fs from 'fs'
 import { GoFilePath} from './interface';
-const token = process.env.FORMDATA_TOKEN; 
+const folder = process.env.FORMDATA_TOKEN;
 const id = process.env.FOLDER_ID;
 @Injectable()
 export class GoFileService{
   constructor(private readonly httpService: HttpService) {}
    async getGoFileServer() : Promise<string> {
     const url = `https://api.gofile.io/getServer`
-    const goFileResponse = this.httpService.axiosRef.get(url);
+    const goFileResponse = await this.httpService.axiosRef.get(url);
     const data = (await goFileResponse).data.data.server; 
     return data; 
   }
   async makeFormData( filepath:string, filename: string){
-    const formData = new FormData(); 
-    formData.append('token', token); 
+    const formData = new FormData();
+    formData.append('token', folder);
     formData.append('folderId', id);
-    formData.append('file', fs.createReadStream(filepath),{
-      filename:filename,
-    }) 
+    formData.append('filepath', fs.createReadStream(filepath));
+    formData.append('filename', filename);
     return formData;
   }
+
    async uploadFormData( fileOne: GoFilePath, fileTwo: GoFilePath ){
     const server = await this.getGoFileServer();
     const urlApi = `https://${server}.gofile.io/uploadFile`;
 
-    const oneFormData = this.makeFormData(fileOne.path, fileOne.name);
-    const twoFormData = this.makeFormData(fileTwo.path, fileTwo.name);
+    const oneFormData = await this.makeFormData(fileOne.path, fileOne.name);
+    const twoFormData = await this.makeFormData(fileTwo.path, fileTwo.name);
     const oneResponse = await this.httpService.axiosRef.post(urlApi, oneFormData, {
-      headers: {'Content-Type': 'multipart/form-data'}
+      headers: {'Content-Type': 'multipart/form-data'},
     });
     const oneData = await oneResponse.data; 
     const twoResponse = await this.httpService.axiosRef.post(urlApi, twoFormData, {
-      headers: {'Content-Type': 'multipart/form-data'}
+      headers: { 'Content-Type': 'multipart/form-data' },
     });
     const twoData = await twoResponse.data; 
     return {oneData, twoData}; 
